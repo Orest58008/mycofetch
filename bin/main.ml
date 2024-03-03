bd7cf328875a598049f93d1f9b9e85364ad1acf8
Orest I <orest58008@yamadachi.quest>
2024-03-03 21:54:12 +0200

    Printing distros yay
    Signed-off-by: Orest I <orest58008@yamadachi.quest>

--- bin/main.ml
+++ bin/main.ml
@@ -13,9 +13,24 @@ let fetched = List.filter (fun x ->
               |> dedup |> List.map (fun x -> x, Fetch.fetch x)
 
 (* Print everything *)
+let logo_counter = ref 0
 let () = List.iter2 (fun source word ->
              match source with
              | "" -> print_string word
+             | "distro" -> let os = Files.retrieve_file "/etc/os-release" '=' in
+                           let id = List.assoc "id" os in
+                           let distro = Distros.distro_of_id id in
+                           (match word with
+                            | "pm_name" -> print_string distro.pm.name
+                            | "pm_command" -> print_string distro.pm.command
+                            | "color" -> print_string distro.color
+                            | "logo_tiny" -> print_string distro.logo_tiny
+                            | "logo" -> (distro.logo.(!logo_counter) ^ "\x1b[0m")
+                                        |> Template.style_line |> String.concat ""
+                                        |> print_string;
+                                        logo_counter := !logo_counter + 1
+                            | _ -> raise (Invalid_argument word)
+                           )
              | source when List.mem_assoc source fetched ->
                 print_string (Hashtbl.find (List.assoc source fetched) word)
              | _ -> print_string word
--- bin/template.ml
+++ bin/template.ml
@@ -3,13 +3,13 @@ let find array elem =
   match index_opt with | Some i -> i | None -> 0
 
 (* Config processing *)
-let process_line ?(style=false) line : string list * string list =
+let process_line line : string list * string list =
   let fonts = [|"c"; "b"; "d"; "i"; "u"|] in (* clear, bold, dim, italic, underlined *)
   let colours = Array.init 8 (fun x -> "c" ^ string_of_int x) in
   let line_split = String.split_on_char '#' line  in
   let rec process_split split sources values = (
       match split with
-      | [] -> (List.rev sources, List.rev values)
+      | [] -> List.rev sources, List.rev values
       | word::rest when String.ends_with ~suffix:"\\" word && List.length rest > 0
         -> (* "\#" processing *)
          let word_trimmed = (String.sub word 0 (String.length word - 1)) in
@@ -22,7 +22,7 @@ let process_line ?(style=false) line : string list * string list =
          let font_num = find fonts word in
          let font = "\x1b[" ^ (string_of_int font_num) ^ "m" in
          process_split rest (""::sources) (font::values)
-      | word::rest when ((String.contains word ':') && (not style)) -> (* pair processing *)
+      | word::rest when (String.contains word ':') -> (* pair processing *)
          let word_list = String.split_on_char ':' word in
          let source = List.hd word_list in
          let value = List.tl word_list |> List.hd in
@@ -30,3 +30,26 @@ let process_line ?(style=false) line : string list * string list =
       | word::rest -> (* ordinary word processing *)
          process_split rest (""::sources) (word::values)
     ) in process_split line_split [] []
+
+let style_line line : string list =
+  let fonts = [|"c"; "b"; "d"; "i"; "u"|] in (* clear, bold, dim, italic, underlined *)
+  let colours = Array.init 8 (fun x -> "c" ^ string_of_int x) in
+  let line_split = String.split_on_char '#' line  in
+  let rec process_split split values = (
+      match split with
+      | [] -> List.rev values
+      | word::rest when String.ends_with ~suffix:"\\" word && List.length rest > 0
+        -> (* "\#" processing *)
+         let word_trimmed = (String.sub word 0 (String.length word - 1)) in
+         process_split ((word_trimmed ^ "#")::rest) values
+      | word::rest when Array.mem word colours -> (* colour processing *)
+         let colour_num = find colours word in
+         let colour = "\x1b[3" ^ (string_of_int colour_num) ^ "m" in
+         process_split rest (colour::values)
+      | word::rest when Array.mem word fonts -> (* font processing *)
+         let font_num = find fonts word in
+         let font = "\x1b[" ^ (string_of_int font_num) ^ "m" in
+         process_split rest (font::values)
+      | word::rest -> (* ordinary word processing *)
+         process_split rest (word::values)
+    ) in process_split line_split []
