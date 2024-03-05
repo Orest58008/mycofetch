35fe054fe830690301bc186160adc7e2ebe7fb71
Orest I <orest58008@yamadachi.quest>
2024-03-05 23:18:37 +0200

    Add parallelism, remove slowness(but not completely XP)
    Signed-off-by: Orest I <orest58008@yamadachi.quest>

--- bin/main.ml
+++ bin/main.ml
@@ -3,10 +3,20 @@ let rec dedup list : 'a list =
   match list with | [] -> []
                   | h::t -> let t_dedup = dedup t in
                             if List.mem h t_dedup then t_dedup else h::t_dedup
-                       
-(* Process config *)
-let template_path = (Sys.getenv "HOME") ^ "/.mftemplate"
-let sources, words = Files.read_file template_path |> Template.process_line
+
+(* Get and process template *)
+let template = (Sys.getenv "HOME") ^ "/.mftemplate" |> Files.read_file
+let sources, words = Template.process_line template
+
+(* Prefetch pm_count if needed *)
+let pm_count = if List.mem "pm_count" words
+               then let id = Files.retrieve_file "/etc/os-release" '=' |> List.assoc "id" in
+                    let distro = Distros.distro_of_id id in
+                    Domain.spawn (fun () -> Unix.open_process_in distro.pm.command
+                                            |> In_channel.input_lines |> List.length )
+               else Domain.spawn (fun () -> 0)
+
+(* Fetch info *)
 let fetched = List.filter (fun x ->
                   try let _ = Fetch.fetch x in true
                   with Invalid_argument _ -> false) sources
@@ -14,19 +24,17 @@ let fetched = List.filter (fun x ->
 
 (* Print everything *)
 let logo_n = ref 1
-let result = Files.read_file template_path |> String.length |> Buffer.create
+let result = String.length template |> Buffer.create
 let result_append = Buffer.add_string result
 let handle source word =
   match source with
   | "" -> result_append word
-  | "distro" -> let os = Files.retrieve_file "/etc/os-release" '=' in
-                let id = List.assoc "id" os in
+  | "distro" -> let id = Files.retrieve_file "/etc/os-release" '=' |> List.assoc "id" in
                 let distro = Distros.distro_of_id id in
                 (match word with
                  | "pm_name" -> result_append distro.pm.name
                  | "pm_command" -> result_append distro.pm.command
-                 | "pm_count" -> let count = Unix.open_process_in distro.pm.command
-                                             |> In_channel.input_lines |> List.length in
+                 | "pm_count" -> let count = Domain.join pm_count in
                                  result_append (string_of_int (count + 1))
                  | "color" -> result_append distro.color
                  | "logo_tiny" -> result_append distro.logo_tiny
@@ -35,7 +43,7 @@ let handle source word =
                                              else (distro.logo.(0) ^ "\x1b[0m]") in
                              Template.style_line logo_line |> String.concat "" |> result_append;
                              logo_n := !logo_n + 1
-                 | _ -> raise (Invalid_argument word)
+                 | _ -> raise (Invalid_argument ("distro" ^ ":" ^ word))
                 )
   | "env" -> result_append (Sys.getenv word)
   | source when List.mem_assoc source fetched ->
