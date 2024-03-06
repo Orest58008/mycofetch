8eea904565d2b3fd0f45d19a827f319ea79500bf
Orest I <orest58008@yamadachi.quest>
2024-03-06 18:38:08 +0200

    Add more parallelism and more List.map
    Signed-off-by: Orest I <orest58008@yamadachi.quest>

--- bin/main.ml
+++ bin/main.ml
@@ -6,27 +6,29 @@ let rec dedup list : 'a list =
 
 (* Get and process template *)
 let template = (Sys.getenv "HOME") ^ "/.mftemplate" |> Files.read_file
-let sources, words = Template.process_line template
+let template_processed = Template.process_line template
 
 (* Prefetch pm_count if needed *)
-let pm_count = if List.mem "pm_count" words
+let pm_count = if List.mem ("distro", "pm_count") template_processed
                then let id = Files.retrieve_file "/etc/os-release" '=' |> List.assoc "id" in
                     let distro = Distros.distro_of_id id in
                     Domain.spawn (fun () -> Unix.open_process_in distro.pm.command
                                             |> In_channel.input_lines |> List.length )
                else Domain.spawn (fun () -> 0)
 
-(* Fetch info *)
+(* Fetch everything *)
+let sources = List.map (fun (x,_) -> x) template_processed
 let fetched = List.filter (fun x ->
                   try let _ = Fetch.fetch x in true
                   with Invalid_argument _ -> false) sources
-              |> dedup |> List.map (fun x -> x, Fetch.fetch x)
+              |> dedup |> List.map (fun x -> x, Domain.spawn (fun() -> Fetch.fetch x))
 
 (* Print everything *)
 let logo_n = ref 1
 let result = String.length template |> Buffer.create
 let result_append = Buffer.add_string result
-let handle source word =
+
+let handle (source, word) =
   match source with
   | "" -> result_append word
   | "distro" -> let id = Files.retrieve_file "/etc/os-release" '=' |> List.assoc "id" in
@@ -41,24 +43,19 @@ let handle source word =
                  | "logo" -> let logo_line = if !logo_n < Array.length distro.logo
                                              then (distro.logo.(!logo_n) ^ "\x1b[0m")
                                              else (distro.logo.(0) ^ "\x1b[0m]") in
-                             Template.style_line logo_line |> String.concat "" |> result_append;
+                             Template.process_line ~style:true logo_line
+                             |> List.map (fun (_, y) -> y) |> String.concat "" |> result_append;
                              logo_n := !logo_n + 1
                  | _ -> raise (Invalid_argument ("distro" ^ ":" ^ word))
                 )
   | "env" -> result_append (Sys.getenv word)
   | source when List.mem_assoc source fetched ->
-     let value = Hashtbl.find_opt (List.assoc source fetched) word in (
+     let value = Hashtbl.find_opt (List.assoc source fetched |> Domain.join) word in (
          match value with
          | Some v -> result_append v
          | None -> raise (Invalid_argument (source ^ ":" ^ word))
        )
   | _ -> raise (Invalid_argument (source ^ ":" ^ word))
-
-(*
-let () = List.iter (fun (x, y) -> print_endline ("\n" ^ (String.capitalize_ascii x));
-                                  Hashtbl.iter (fun a b -> print_endline (a ^ ":" ^ b)) y)
-           fetched
- *)
            
-let () = List.iter2 handle sources words
+let () = List.iter handle template_processed
 let () = Buffer.output_buffer stdout result
--- bin/template.ml
+++ bin/template.ml
@@ -3,66 +3,33 @@ let find array elem =
   match index_opt with | Some i -> i | None -> 0
 
 (* Config processing *)
-let process_line line : string list * string list =
-  let line = Str.global_replace (Str.regexp "$") "\x1b[0m" line in (* clear styling at each '\n' *)
-  let line = Str.global_replace (Str.regexp "#+") "#" line in (* replace any number of '#' with one '#' *)
+let process_line ?(style=false) line : (string * string) list =
+  let line = Str.global_replace (Str.regexp "$") "\x1b[0m" line in (* clear styling at each \n *)
   let fonts = [|"c"; "b"; "d"; "i"; "u"|] in (* clear, bold, dim, italic, underlined *)
   let colours = Array.init 8 (fun x -> "c" ^ string_of_int x) in
   let line_split = (String.split_on_char '#' line) in
-  let rec process_split split sources values = (
-      match split with
-      | [] -> List.rev sources, List.rev values
-      | word::rest ->
-         if String.ends_with ~suffix:"\\" word && List.length rest > 0 then (* '\#' processing *)
-           let word_trimmed = (String.sub word 0 (String.length word - 1)) in
-           process_split ((word_trimmed ^ "#")::rest) sources values
-         else if word = "cauto" then (* automatic colour detection *)
-           let os = Files.retrieve_file "/etc/os-release" '=' in
-           let id = List.assoc "id" os in
-           let distro = Distros.distro_of_id id in
-           process_split (distro.color::rest) sources values
-         else if Array.mem word colours then (* colour processing *)
-           let colour_num = find colours word in
-           let colour = "\x1b[3" ^ (string_of_int colour_num) ^ "m" in
-           process_split rest (""::sources) (colour::values)
-         else if Array.mem word fonts then (* font processing *)
-           let font_num = find fonts word in
-           let font = "\x1b[" ^ (string_of_int font_num) ^ "m" in
-           process_split rest (""::sources) (font::values)
-         else if (String.contains word ':') then (* pair processing *)
-           let word_list = String.split_on_char ':' word in
-           let source = List.hd word_list in
-           let value = List.tl word_list |> List.hd in
-           process_split rest (source::sources) (value::values)
-         else
-           process_split rest (""::sources) (word::values)
-    ) in process_split line_split [] []
-
-let style_line line : string list =
-  let line = Str.global_replace (Str.regexp "$") "\x1b[0m" line in (* clear styling at each '\n' *)
-  let fonts = [|"c"; "b"; "d"; "i"; "u"|] in (* clear, bold, dim, italic, underlined *)
-  let colours = Array.init 8 (fun x -> "c" ^ string_of_int x) in
-  let line_split = String.split_on_char '#' line  in
-  let rec process_split split values = (
-      match split with
-      | [] -> List.rev values
-      | word::rest ->
-         if String.ends_with ~suffix:"\\" word && List.length rest > 0 then (* '\#' processing*)
-           let word_trimmed = (String.sub word 0 (String.length word - 1)) in
-           process_split ((word_trimmed ^ "#")::rest) values
-         else if word = "cauto" then (* automatic colour detection *)
-           let os = Files.retrieve_file "/etc/os-release" '=' in
-           let id = List.assoc "id" os in
-           let distro = Distros.distro_of_id id in
-           process_split (distro.color::rest) values
-         else if Array.mem word colours then (* colour processing *)
-           let colour_num = find colours word in
-           let colour = "\x1b[3" ^ (string_of_int colour_num) ^ "m" in
-           process_split rest (colour::values)
-         else if Array.mem word fonts then (* font processing *)
-           let font_num = find fonts word in
-           let font = "\x1b[" ^ (string_of_int font_num) ^ "m" in
-           process_split rest (font::values)
-         else
-           process_split rest (word::values)
-    ) in process_split line_split []
+  let rec process_chunk ?(style=false) chunk : string * string =
+    if String.ends_with ~suffix:"\\" chunk then (* "\#" handling *)
+      let chunk_trimmed = (String.sub chunk 0 (String.length chunk - 1)) in
+      "", (chunk_trimmed ^ "#")
+    else if chunk = "cauto" then (* cauto handling *)
+      let distro = Files.retrieve_key "/etc/os-release" '=' "id" |> Distros.distro_of_id in
+      process_chunk distro.color
+    else if Array.mem chunk colours then
+      let colours = Array.init 8 (fun x -> "c" ^ string_of_int x) in
+      let colour_num = find colours chunk in
+      let colour = "\x1b[3" ^ (string_of_int colour_num) ^ "m" in
+      "", colour
+    else if Array.mem chunk fonts then
+      let fonts = Array.init 8 (fun x -> "c" ^ string_of_int x) in
+      let font_num = find fonts chunk in
+      let font = "\x1b[3" ^ (string_of_int font_num) ^ "m" in
+      "", font
+    else if (String.contains chunk ':') && (not style) then
+      let pair = String.split_on_char ':' chunk in
+      let source = List.hd pair in
+      let word = List.tl pair |> List.hd in
+      source, word
+    else
+      "", chunk
+  in List.map (process_chunk ~style:style) line_split
